ea06aacf033b670f09cb7c6ad7cc31265fc22d3e
DA105 <gg998571@gmail.com>
2020-01-18 13:45:08 +0800

    init commit

---
+++ HomeWork39_2_3.java
@@ -0,0 +1,13 @@
+package rickie;
+
+public class HomeWork39_2_3 {
+	public void main (String[] args ) {
+		int i = 1 , sum = 0;	
+		while(i <= 10) {
+			sum *= i;
+			i++;
+			System.out.println(sum);
+		}
+	}
+
+}
